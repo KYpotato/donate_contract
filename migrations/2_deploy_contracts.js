030e2beaf3062df8fb3a118f826ea456e675b234
KYpotato <s0methingfun313@gmail.com>
2019-05-30 13:03:15 +0900

    fix deploy error

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 const Donation = artifacts.require("Donation");
 
 module.exports = function(deployer) {
-  deployer.deploy(Donation, 100, 0.01, 0.1, 0.005, 1, 0.5);
+  deployer.deploy(Donation, 
+    100, // term
+    web3.utils.toWei("0.01", "ether"),  // min
+    web3.utils.toWei("0.1", "ether"),   // max
+    web3.utils.toWei("0.005", "ether"), // unit
+    web3.utils.toWei("1", "ether"),     // upper limit
+    web3.utils.toWei("0.5", "ether"));  // lower limit
 };
